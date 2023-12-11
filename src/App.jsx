ad63362867e8076e1ba929b92be206861126b8ce
stevediggs70 <stevediggs670@gmail.com>
2023-12-11 15:54:05 -0500

    baselined route in App.jsx, baselined header nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
 import { useState } from 'react'
 
 import './App.css'
+// Components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom"
+
+// Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
+
 
 export default App
--- src/components/header.js
+++
@@ -1,7 +0,0 @@
-// PATH: components/header.js
-
-function Header(props) {
-    return <h1>Header</h1>;
-  }
-  
-  export default Header;
