1bc20c0a1fe04f93ac5ede3c9b44f7d7957c7fa6
stevediggs70 <stevediggs670@gmail.com>
2023-12-11 20:57:14 -0500

    skeleton complete

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,38 @@
-function About(props) {
-    return <h1>About</h1>;
-  }
+// PATH:  pages/about.jsx
+import { useState, useEffect } from "react"
+
+
+function About() {
+    // 1. create state to hold about data
+    const [about, setAbout] = useState(null);
   
-  export default About;
+    // 2. create function to make api call
+    const getAboutData = async () => {
+  
+      // 3a. make api call and get response
+      const response = await fetch("./about.json")
+
+      // 3b. turn response into javascript object
+      const data = await response.json();
+
+    // 3c. set the about state to the data
+      setAbout(data);
+};
+
+// 3. make an initial call for the data inside a useEffect, so it only happens once on component load
+useEffect(() => { getAboutData() } , []);
+
+// define a function that will return the JSX needed once we get the data
+const loaded = () => (
+<div>
+  <h2>{about.name}</h2>
+  <h3>{about.email}</h3>
+  <p>{about.bio}</p>
+</div>
+);
+
+// if data arrives return the result of loaded, if not, an h1 that says loading
+return about ? loaded() : <h1>Loading...</h1>;
+}
+
+export default About;
